99a53241716da68c0e99c2f919e350eb0938d20a
Lukas Valach <luvalach@seznam.cz>
2016-02-18 09:18:38 +0100

    Letter opener web

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,16 +84,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener_web
 
   # my personal fake account
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => "587",
-      :domain => "gmail.com",
-      :user_name => "374027rails@gmail.com",
-      :password => "374027Rails!",
-      :authentication => "plain",
-      :enable_starttls_auto => true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #    :address => "smtp.gmail.com",
+  #    :port => "587",
+  #    :domain => "gmail.com",
+  #    :user_name => "374027rails@gmail.com",
+  #    :password => "374027Rails!",
+  #    :authentication => "plain",
+  #    :enable_starttls_auto => true
+  # }
 
 
 end
